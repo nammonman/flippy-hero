dce4265e94046ab31a12ba76a987cc67d34ef5ca
nammonman <sirapat69@outlook.com>
2025-04-10 20:40:16 +0700

    a

--- flippyhero/src/app/page.js
+++ flippyhero/src/app/page.js
@@ -22,13 +22,13 @@ function DPad({ isWideScreen }) {
   const btnSize = isWideScreen ? 'h-[8vh] w-[8vh]' : 'h-[7vh] w-[7vh]';
   return (
     <div className={`relative w-[32vh] left-0 flex flex-col items-center sm:text-xl text-lg font-black`}>
-      <button className={`relative ${btnSize} bg-gray-500 rounded shadow-amber-950 shadow-md active:shadow-sm`}>U</button>
+      <button className={`relative ${btnSize} bg-gray-500 rounded shadow-amber-900 shadow-md active:shadow-sm`}>U</button>
       <div className="flex">
-        <button className={`relative ${btnSize} bg-gray-500 rounded shadow-amber-950 shadow-md active:shadow-sm`}>L</button>
+        <button className={`relative ${btnSize} bg-gray-500 rounded shadow-amber-900 shadow-md active:shadow-sm`}>L</button>
         <div className={`relative z-10 ${btnSize} bg-gray-500`}></div>
-        <button className={`relative ${btnSize} bg-gray-500 rounded shadow-amber-950 shadow-md active:shadow-sm`}>R</button>
+        <button className={`relative ${btnSize} bg-gray-500 rounded shadow-amber-900 shadow-md active:shadow-sm`}>R</button>
       </div>
-      <button className={`relative ${btnSize} bg-gray-500 rounded shadow-amber-950 shadow-md active:shadow-sm`}>D</button>
+      <button className={`relative ${btnSize} bg-gray-500 rounded shadow-amber-900 shadow-md active:shadow-sm`}>D</button>
     </div>
   );
 }
@@ -38,11 +38,11 @@ function ActionButtons({ isWideScreen }) {
   return (
     <div className="flex flex-row gap-[5%] w-[32vh]"> 
       <div className="relative w-full right-0 flex flex-col items-center space-y-[20%] text-lg font-black">
-        <button className={` ${btnSize} bg-green-500 rounded-full shadow-amber-950 shadow-md active:shadow-sm`}>Z</button>
-        <button className={` ${btnSize} bg-blue-500 rounded-full shadow-amber-950 shadow-md active:shadow-sm`}>C</button>
+        <button className={` ${btnSize} bg-green-500 rounded-full shadow-amber-900 shadow-md active:shadow-sm`}>Z</button>
+        <button className={` ${btnSize} bg-blue-500 rounded-full shadow-amber-900 shadow-md active:shadow-sm`}>C</button>
       </div>
       <div className="relative w-full right-0 flex items-center space-y-[9%] space-x-[9%] text-lg font-black">
-        <button className={` ${btnSize} bg-red-500 rounded-full shadow-amber-950 shadow-md active:shadow-sm`}>A</button>
+        <button className={` ${btnSize} bg-red-500 rounded-full shadow-amber-900 shadow-md active:shadow-sm`}>A</button>
       </div>
     </div>
   );
@@ -88,7 +88,7 @@ export default function Home() {
           <div className='flex justify-between items-center m-[5%] '>
           <GameGrid isWideScreen={isWideScreen}/>
           </div>
-          <div className='flex justify-between items-center mx-[5%] space-x-[3%] my-[20%]'>
+          <div className='flex justify-between items-center mx-[5%] space-x-[3%] my-[15%]'>
             <DPad isWideScreen={isWideScreen}/>
             <ActionButtons isWideScreen={isWideScreen}/>
           </div>
