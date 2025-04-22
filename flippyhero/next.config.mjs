5add9c71cb8ff4df6269e4348b8d4a48ec813c68
nammonman <sirapat69@outlook.com>
2025-04-22 23:49:46 +0700

    ?

--- flippyhero/next.config.mjs
+++ flippyhero/next.config.mjs
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
+const nextConfig = {
+    async headers() {
+        return [
+            {
+                source: '/(.*).png',
+                headers: [
+                    {
+                        key: 'Cache-Control',
+                        value: 'public, max-age=360000, immutable'
+                    }
+                ]
+            }
+        ]
+    }
+};
 export default nextConfig;
